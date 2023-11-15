999a5fef3993dc6e5d69be0921d40d6fdc72d626
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2023-11-15 23:04:28 +0300

    Explicitly and implicitly(interface)

--- Module10/Program.cs
+++ Module10/Program.cs
@@ -6,7 +6,38 @@ namespace Module10
     {
         static void Main(string[] args)
         {
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
 
+            Worker worker = new Worker();
+            //worker.Build();
+            ((IWorker)worker).Build();
         }
+    }
+
+    public class Writer : IWriter
+    {
+        void IWriter.Write() 
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IWriter
+    {
+        void Write();
+    }
+
+    public class Worker : IWorker
+    {
+        void IWorker.Build()
+        {
+            throw new Exception();
+        }
+    }
+
+    public interface IWorker
+    {
+        public void Build();
     }
 }
